be1696872ec4850b0f587bea18e8339ab39294a5
cncg <ccalvo0314@gmail.com>
2023-03-03 19:37:36 -0500

    reset feature built into save button function

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let count = 0; //? what is this? this is ONE of the ways to declare a variable in javascript. With the let keyword. We declared the variable 'count' and set it to 0.
-console.log(count);
 
 let countEl = document.getElementById("count-el"); //? what is in-between the ""? A string. A set of text, words or characters. We also grabbed an id from an element.
-console.log(countEl);
 
 //? Here we created a function called increment(). Functions usually preform a task or executes some sort of procedure that we declare.
 function increment() {
@@ -12,10 +10,13 @@ function increment() {
   countEl.innerText = count;
 }
 
-let saveEl = document.getElementById("save-el");
-console.log(saveEl);
+let saveEl = document.getElementById("save-el"); //? here we are making a save button grab the ID in the html similar to our count-el.
 
 function save() {
+  //? This save function is going to save the increment and display it on the page. Remember the += trick!
   let countStr = count + " - ";
   saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
+//? the save feature will reset the count back to zero when the save button is clicked!
